855c5d8cc38b7b2f49d0964939669c1be98fed18
animeze <130531179+animeze@users.noreply.github.com>
2023-11-17 20:03:25 +0545

    Update datebase.py

--- datebase.py
+++ datebase.py
@@ -1,27 +1,64 @@
-import python
+import python    
+import time 
+
+def get_readable_time(seconds: int) -> str:
+    count = 0
+    ping_time = ""
+    time_list = []
+    time_suffix_list = ["s", "m", "h", "days"]
+
+    while count < 4:
+        count += 1
+        if count < 3:
+            remainder, result = divmod(seconds, 60)
+        else:
+            remainder, result = divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+
+    for x in range(len(time_list)):
+        time_list[x] = str(time_list[x]) + time_suffix_list[x]
+    if len(time_list) == 4:
+        ping_time += time_list.pop() + ", "
+
+    time_list.reverse()
+    ping_time += ":".join(time_list)
+
+    return ping_time
+    
+    
+HELP_STRINGS = """
+🎛 *Here you can get all of the Help Commands!*
+
+ ❍ /help: PM's you this message.
+ ❍ /help <module name>: PM's you info about that module.
+ ❍ /donate: information on how to donate!
+ ❍ /settings:
+  in PM: will send you your settings for all supported modules.
+  in a group: will redirect you to pm, with all that chat's settings.
+ 
+All commands can either be used with / or !.
+And the following:
+"""
+
+
+
+Start = """ 
+I am Bots develpoders 
+Made By shanks
+"""
+
 
 @Bot.on_message(filters.command('start') & filters.private)
 reply(client: Client, message: Message):
-    buttons = [
-        [
-            InlineKeyboardButton(
-                "Join Channel",
-                url = "t.me/Shanks_Kun")
-        ]
-    ]
-    try:
-        buttons.append(
-            [
-                InlineKeyboardButton(
-                    text = 'Try Again',
-                    url = f"https://t.me/Wizardz_Bot"
-                )
-            ]
-          ),
-        reply_markup = InlineKeyboardMarkup(buttons),
-        quote = True,
-        disable_web_page_preview = True
-        
+print(Start)
+
+@Bot.on_message(filters.command('help') & filters.private)
+reply(client: Client, message: Message):
+print(HELP_STRINGS)
+
     if __name__ == '__main__':
     LOGGER.info("Successfully loaded modules: " + str(ALL_MODULES))
     telethn.start(bot_token=TOKEN)
