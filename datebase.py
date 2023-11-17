c7e0a5ba7c71401ec0014d6606ebf1f847675f02
animeze <130531179+animeze@users.noreply.github.com>
2023-11-17 19:04:01 +0545

    Update datebase.py

--- datebase.py
+++ datebase.py
@@ -17,19 +17,8 @@ reply(client: Client, message: Message):
                     url = f"https://t.me/Wizardz_Bot"
                 )
             ]
-        )
-    except IndexError:
-        pass
-
-    await message.reply(
-        text = FORCE_MSG.format(
-                first = message.from_user.first_name,
-                last = message.from_user.last_name,
-                username = None if not message.from_user.username else '@' + message.from_user.username,
-                mention = message.from_user.mention,
-                id = message.from_user.id
-            ),
+          ),
         reply_markup = InlineKeyboardMarkup(buttons),
         quote = True,
         disable_web_page_preview = True
-    )
+    
